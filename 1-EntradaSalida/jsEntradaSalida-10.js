5f86ba730f95f1bc9231a6bf52d5b69bf9da1367
Zurita00 <52494807+Zurita00@users.noreply.github.com>
2019-07-04 11:05:19 -0300

    Add files via upload
    Ejercicios resueltos

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe;
+    var descuento;
+    var resultado;
+
+	importe = parseInt(document.getElementById("importe").value);
+    descuento = parseInt(25*100/importe);
+    resultado = importe - descuento;
+
+    document.getElementById("resultado").value = resultado;
 }
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,16 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    var numero1; 
+    numero1 = document.getElementById("numeroUno").value;
+    var numero2;
+    numero2 = document.getElementById("numeroDos").value;
+    var numero3;
 
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    numero3 = numero1 + numero2;
+    
+    alert("La suma es " + numero3);
 }
 
