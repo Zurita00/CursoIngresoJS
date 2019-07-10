f55dd3e7eda8c6365872e724d3433cbdf9c5a3e7
Zurita00 <52494807+Zurita00@users.noreply.github.com>
2019-07-10 12:20:41 -0300

    Add files via upload
    Ejercicios de If

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,31 @@
-function mostrar()
-{
-//tomo la edad  
+function mostrar() {
+    //tomo la edad  
 
+    var edad;
+    edad = parseInt(document.getElementById("edad").value);
 
+    if (edad >= 18) { //Si la edad es mayor o igual a 18 es un adulto
 
+        alert("Es un adulto");
 
+    }
+
+    else { // Entonces si es falso es un adolescente o un niño
+        if (edad >= 13 && edad <= 17) { //Si es mayor de 13 y menor de 17 es adolescente
+
+            alert("Es adolescente");
+
+        }
+
+        else { //Si es falso que es un adolescente entonces es un niño
+
+            alert("Es un niño")
+        }
+
+    }
+
+// if (condicion){        Otra posiblidad para cuando son mas que una condicion 
+// }                        sin necesidad de demasiados else o if
+// else if (condicion) {   
+//}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,13 @@
-function mostrar()
-{
-//tomo la edad  
+function mostrar() {
+    //tomo la edad  
+    var edad;
+    edad = parseInt(document.getElementById("edad").value);
 
-	
+    document.getElementById("estadoCivil").value = "Soltero"
 
+    if (edad < 18) {
+
+        alert("Es muy pequeño para no ser soltero")
+    }
 
 }//FIN DE LA FUNCIÓN
