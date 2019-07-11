1107c29d89281caa4ffd418d72af7373d5c64472
Zurita00 <52494807+Zurita00@users.noreply.github.com>
2019-07-11 12:28:51 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,15 @@
-function mostrar()
-{
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-//alert (mesDelAño);
-	
-	
+function mostrar() {
+    var mes = document.getElementById('mes').value;
 
 
+    switch (mes) {
+
+        case "Febrero":
+            alert("Este mes no tiene más de 29 días");
+            break;
+
+       default:
+            alert("Este mes tiene 30 días o más");
+
+    }
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,11 +1,25 @@
-function mostrar()
-{
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+function mostrar() {
 
-//alert (mesDelAño);
-	
-	
+    var mes = document.getElementById('mes').value;
+
+
+    switch (mes) {
+        case "Febrero":
+            alert("Tiene 28 días");
+            break;
+        case "Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+            alert("Tiene 30 días");
+            break;
+
+        default:
+            alert("Tiene 31 días");
+            break;
+
+    }
+
 
 
 
