c6abb96f91f4a7a3417318097d0ef3d690a17f0e
Zurita00 <52494807+Zurita00@users.noreply.github.com>
2019-08-03 10:39:27 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,80 +1,76 @@
-function mostrar()
-{
-//es la misma vrga pero con diferentes palabras
-        var letra;
-        var num;
-        var seguir;
-        var numPar = 0;
-        var numImpar = 0;
-        var ceros = 0;
-        var promedio = 0;
-        var numPos = 0;
-        var contPos = 0;
-        var numNeg=0;
-        var minimo;
-        var maximo;
-        var lMinimo;
-        var lMaximo;
-        var flag = 0;
-    
-        do {
-    
-            letra = prompt("Ingrese una letra: ");
-            num = parseInt(prompt("Ingrese un numero entre el -100 y 100: "));
-    
-            while (num < -100 || num > 100 && isNaN(num)) {
-    
-                num = parseInt(prompt("Reingrese un numero: "));
-            }
-            seguir = prompt("¿Quiere seguir?");
-    
-            if (num % 2 == 0) {
-    
-                numPar++
-            }
-            else {
-                numImpar++
-            }
-    
-    
-            if (num > 0) {
-    
-                numPos += num
-                contPos ++
-            }
-            else if (num < 0) {
-    
-                numNeg+=num
-    
-            }
-    
-            else {
-                ceros++
-            }
-    
-            if( num < minimo || flag == 0){
-                minimo = num
-                lMinimo = letra
-            }
-            if ( num > maximo || flag == 0 ){
-                maximo = num
-                lMaximo = letra
-    
-            }
-    
-        } while (seguir == 's');
-    
-    if( contPos !=0 ){
-        promedio = numPos / contPos;
-    }
-    
-    document.write("Cantidad de numeros pares: " + numPar + "<br>");
-    document.write("Cantidad de numeros impares: " + numImpar + "<br>");
-    document.write("Cantidad de ceros: " + ceros + "<br>");
-    document.write("Promedio de los numeros positivos: "+ promedio + "<br>");
-    document.write("Suma de los numeros negativos:" + numNeg + "<br>");
-    document.write("Numero y letra del maximo: " + maximo + " " + lMaximo + "<br>");
-    document.write("Numero y letra del minimo: " + minimo + " " + lMinimo + "<br>");
-    
+function mostrar() {
+
+    var marca;
+    var peso;
+    var temperatura;
+    var seguir;
+    var cantidadPeso;
+    var contPeso = 0
+    var tempPar = 0;
+    var marPesado;
+    var contMar = 0;
+    var productos = 0;
+    var minimo = 0;
+    var maximo = 0;
+    var promedio = 0;
+    var flag = 0;
+
+    do {
+
+        marca = prompt("Ingrese la marca: ");
+        peso = parseInt(prompt("Ingrese el peso entre el 1 y 100: "));
+        while (peso < 1 || peso > 100 && isNaN(peso)) {
+
+            peso = parseInt(prompt("Reingrese el peso: "));
+        }
+        temperatura = parseInt(prompt("Ingrese la temperatura: "));
+        while (temperatura < -30 || temperatura > 30 && isNaN(temperatura)) {
+
+            temperatura = parseInt(prompt("Reingrese la temperatura: "));
+        } seguir = prompt("¿Quiere seguir?");
+        
+        if( peso > 1 && peso < 100){
+            cantidadPeso = peso;
+            contPeso++
+        }
+
+        if (temperatura % 2 == 0) {
+
+            tempPar++
+        }
+        if (peso > 0) {
+
+            marPesado = marca;
+            contMar++
+        }
+
+        if (temperatura < 0) {
+
+            productos++;
+        }
+
+        if (peso < minimo || flag == 0) {
+            minimo = peso;
+        }
+        if (peso > maximo || flag == 0) {
+            maximo = peso;
+            flag=1;
+        }
+        
+
+    } while (seguir == 's');
+
+
+    if (contPeso != 0) {
+        promedio = cantidadPeso /contPeso ;
     }
 
+   document.write("Cantidad de temperaturas pares: " + tempPar + "<br>" );
+   document.write("La marca del producto mas pesado: " + marPesado + "<br>");
+   document.write("Cantidad de productos que se conservan a menos de 0 grados: " + productos + "<br>");
+   document.write("El promedio del peso de todos los productos: " + promedio + "<br>");
+   document.write("El peso máximo: " + maximo + "<br>");
+   document.write("El peso minimo: " + minimo + "<br>");
+    
+
+}
